8f3cf1fd5f9dd072fe84f39b17d2fa167a62d4fe
Hamza Bin Ahmed Makharish <hamzaahmed.5858@gmail.com>
2023-03-03 20:21:40 +0500

    done

---
+++ main.dart
@@ -0,0 +1,224 @@
+import 'dart:io';
+
+void main() {
+  // print('Answer 1');
+  // stdout.write('enter any number');
+  // int? number1 = int.tryParse(stdin.readLineSync()!);
+
+  // int factorial = 1;
+  // for (int i =1; i <= number1!; i++) {
+  //   factorial = factorial * i;
+  // }
+  // print('factorial of $number1 is $factorial');
+
+  // print('Answer 2');
+
+  // List<int> number2 = [45,61,24,45,81,65,14];
+  // int maxNumber=number2[0];
+  // for(int i=1;i<number2.length;i++){
+  //   if(number2[i]>maxNumber){
+  //     maxNumber=number2[i];
+  //   }
+  // }
+  // print(maxNumber);
+
+  // print('The highest number is $maxNumber');
+
+  // print('Answer 3');
+
+  // List<int> number3 = [
+  //   0,
+  //   3,
+  //   5,
+  //   7,
+  //   9,
+  //   11,
+  //   13,
+  //   15,
+  //   17,
+  //   19,
+  //   21,
+  //   23,
+  //   25,
+  //   27,
+  //   29,
+  // ];
+
+  // for (int i = 1; i < 10; i++) {
+  //   print(number3[i]);
+  // }
+
+  // print('Answer 4');
+
+  // print('part 1');
+  // for (int i = 1; i <5; i++) {
+  //   for (int j = 0; j <10 ; j++) {
+  //     stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+// print('part 2');
+  // int star2 = 5;
+  // String output = '';
+  // for (int i = 1; i <= star2; i++) {
+  //   output += '*';
+  //   print(output);
+  // }
+
+  // print('part 3');
+  // int count = 1;
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= count; j++) {
+  //     stdout.write("*");
+  //   }
+  //   print('');
+  //   count += 2;
+  // }
+
+  // print('Answer 5');
+
+  // List<int> hundred = [
+  //   1,
+  //   2,
+  //   3,
+  //   4,
+  //   5,
+  //   6,
+  //   7,
+  //   8,
+  //   9,
+  //   10,
+  //   11,
+  //   12,
+  //   13,
+  //   14,
+  //   15,
+  //   16,
+  //   17,
+  //   18,
+  //   19,
+  //   20,
+  //   21,
+  //   22,
+  //   23,
+  //   24,
+  //   25,
+  //   26,
+  //   27,
+  //   28,
+  //   29,
+  //   30,
+  //   31,
+  //   32,
+  //   33,
+  //   34,
+  //   35,
+  //   36,
+  //   37,
+  //   38,
+  //   39,
+  //   40,
+  //   41,
+  //   42,
+  //   43,
+  //   44,
+  //   45,
+  //   46,
+  //   47,
+  //   48,
+  //   49,
+  //   50,
+  //   51,
+  //   52,
+  //   53,
+  //   54,
+  //   55,
+  //   56,
+  //   57,
+  //   58,
+  //   59,
+  //   60,
+  //   61,
+  //   62,
+  //   63,
+  //   64,
+  //   65,
+  //   66,
+  //   67,
+  //   68,
+  //   69,
+  //   70,
+  //   71,
+  //   72,
+  //   73,
+  //   74,
+  //   75,
+  //   76,
+  //   77,
+  //   78,
+  //   79,
+  //   80,
+  //   81,
+  //   82,
+  //   83,
+  //   84,
+  //   85,
+  //   86,
+  //   87,
+  //   88,
+  //   89,
+  //   90,
+  //   91,
+  //   92,
+  //   93,
+  //   94,
+  //   95,
+  //   96,
+  //   97,
+  //   98,
+  //   99,
+  //   100,
+  // ];
+
+  // List<int> empty = [];
+
+  // for (int i = 0; i < hundred.length; i++) {
+  //   if (hundred[i] % 2 == 0) {
+  //     empty.addAll([i]);
+  //   }
+  // }
+
+  // int vale = 0;
+
+  // for (int i = 1; i < empty.length; i++) {
+  //   vale += i;
+  // }
+  // print(vale);
+
+  // print('Answer 6');
+  //    var myList = [12,34,56,16,89,67,43];
+
+  //   for(var i=0;i<myList.length/2;i++){
+  //   int rever = myList[i];
+  //       myList[i] = myList[myList.length-1-i];
+  //       myList[myList.length-1-i] = rever;
+  //   }
+
+  //   print(myList);
+
+  // print('Answer 7');
+
+  // int a = 0;
+  // int b = 1;
+  // int c;
+
+  // for (int i = 0; i < 12; i++) {
+  //   print('$a');
+  //   c = a + b;
+  //   a = b;
+  //   b = c;
+  // }
+
+  List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+}
